ba9b3dc0e825676a00946ef42e95ddc0d1295315
dantearrighi <dantea@fibertel.com.ar>
2014-11-30 19:47:50 -0300

    2do Commit
    Aca subo lo realizado la otra noche. Modulo Clientes completo (creo)

---
+++ models/cliente.js
@@ -0,0 +1,98 @@
+var mongoose = require('mongoose'), Schema = mongoose.Schema;
+var clienteSchema = new Schema({
+    nombre: String,
+    apellido: String,
+    dniCuil: Number,
+    claveFiscal: String,
+    mail: String,
+    tel1: String,
+    tel2: String,
+},
+{
+collection: 'clientes'
+});
+
+
+/*clienteSchema.static("crearClienteDoit", function ( nombre,  apellido, dni,  mail,  tel1, tel2, cb) {
+    var clientenuevo = mongoose.model('Cliente', clienteSchema);
+    var cnew = new clientenuevo({ nombre: nombre, apellido: apellido, dni: dni, mail: mail, tel1: tel1, tel2: tel2 });
+    
+    cnew.save(function (err, cnew) {
+        if (err) return console.error(err);
+       
+    }) , function (err, r) {
+        cb(r);
+    };
+});*/
+/*
+function crearCliente(nombre, apellido, dni, mail, tel1, tel2, callback) {
+    
+    var clientenuevo = mongoose.model('Cliente', clienteSchema);
+    var cnew = new clientenuevo({ nombre: nombre, apellido: apellido, dni: dni, mail: mail, tel1: tel1, tel2: tel2 });
+
+    cnew.save(function (err, cnew) {
+        if (err) return console.error(err);
+       
+    });
+
+    };
+
+/*clienteSchema.static("crearCliente", function (cb) {
+    this.constructor({ nombre: "nombre", apellido: "apellido", dni:"dni", mail: "mail", tel1:"tel1", tel2:"tel2"  }, function (err, r) {
+        r.nombre = nombre;
+        r.apelido = apellido;
+        r.dni = dni;
+        r.mail = mail;
+        r.tel1 = tel1;
+        r.tel2 = tel2;
+        r.save(function (err, cl) {
+            cb(cl);
+        });
+
+        cb(r);
+       
+    });
+});
+        
+*/
+
+/*
+personaSchema.static("buscarAlumnos", function (cb) {
+    this.find({ cargo: "Alumno" }, function (err, r) {
+        cb(r);
+    });
+});
+
+*/
+
+clienteSchema.static("buscarClientes", function (cb) {
+    this.find({ }, function (err, r) {
+        cb(r);
+    });
+});
+clienteSchema.static("eliminarcliente", function (id, cb) {
+    this.remove({ _id: id, nombre: "cliente" }, function (err) {
+        cb();
+    });
+});
+clienteSchema.static("obtenercliente", function (id, cb) {
+    this.find({ _id: id, nombre: "cliente" }, function (err, r) {
+        cb(r);
+    });
+});
+clienteSchema.static("editarcliente", function (id, name, cb) {
+    this.findOne({ _id: id, nombre: "cliente" }, function (err, r) {
+        r.nombre = name;
+       
+        r.save(function (err, cl) {
+            cb(cl);
+        });
+    });
+});
+clienteSchema.static("buscarPorNombre", function (name, cb) {
+    this.find({ nombre: name }, function (err, r) {
+        cb(r);
+    });
+});
+
+module.exports = mongoose.model('cliente', clienteSchema); //en teoria, esto se podria llamar desde otro lado.
---
+++ views/main.js
@@ -0,0 +1,24 @@
+﻿
+var app = module.parent.exports.app, vClientes = require('../models/cliente.js');
+
+
+
+app.get('/', function(req, res) {
+    vClientes.buscarClientes(function (pers) {
+        res.render('index', { title: 'Listado', obj: pers });
+    });
+});
+
+app.get('/new', function (req, res) {
+    res.render('new', { title: 'Nuevo', obj: {} });
+});
+
+app.post('/new', function(req, res) {
+    var vNuevoCliente = new vClientes({ nombre: req.body.nombre, apellido: req.body.apellido });
+    vNuevoCliente.save(function (err, vNuevoCliente) {
+        res.redirect("/");
+    });
+});
+
+
+
